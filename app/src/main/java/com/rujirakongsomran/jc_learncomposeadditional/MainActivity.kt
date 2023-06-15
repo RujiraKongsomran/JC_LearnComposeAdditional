620e1187a4b3c5b780849eee75b34dd16b8b780f
Rujira Kongsomran <rujirabiwberry@gmail.com>
2023-06-15 15:49:54 +0700

    FlowRow

--- app/src/main/java/com/rujirakongsomran/jc_learncomposeadditional/MainActivity.kt
+++ app/src/main/java/com/rujirakongsomran/jc_learncomposeadditional/MainActivity.kt
@@ -4,14 +4,26 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.ExperimentalLayoutApi
+import androidx.compose.foundation.layout.FlowRow
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.pager.HorizontalPager
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Check
 import androidx.compose.material3.Card
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FilterChip
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -50,11 +62,35 @@ fun CreateHorizontalPager() {
     }
 }
 
+@OptIn(ExperimentalLayoutApi::class, ExperimentalMaterial3Api::class)
+@Composable
+fun Filters() {
+    val filters = listOf(
+        "Washer/Dryer", "Ramp access", "Garden", "Cats OK",
+        "Dogs OK", "Smoke-free"
+    )
+
+    FlowRow(horizontalArrangement = Arrangement.spacedBy(8.dp)) {
+        filters.forEach { title ->
+            var selected by remember { mutableStateOf(false) }
+            val leadingIcon: @Composable () -> Unit = {
+                Icon(Icons.Default.Check, null)
+            }
+            FilterChip(
+                selected = selected,
+                onClick = { selected = !selected },
+                label = { Text(text = title) },
+                leadingIcon = if (selected) leadingIcon else null
+            )
+        }
+    }
+}
+
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     JC_LearnComposeAdditionalTheme {
-        CreateHorizontalPager()
+        Filters()
     }
 }

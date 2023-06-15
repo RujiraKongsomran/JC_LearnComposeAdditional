5c129a9f7b3c44cd54ec0404dcaa2b8fbeb7bd07
Rujira Kongsomran <rujirabiwberry@gmail.com>
2023-06-15 15:15:48 +0700

    HorizontalPager: create

--- app/src/main/java/com/rujirakongsomran/jc_learncomposeadditional/MainActivity.kt
+++ app/src/main/java/com/rujirakongsomran/jc_learncomposeadditional/MainActivity.kt
@@ -3,13 +3,17 @@ package com.rujirakongsomran.jc_learncomposeadditional
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
+import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.pager.HorizontalPager
+import androidx.compose.material3.Card
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.rujirakongsomran.jc_learncomposeadditional.ui.theme.JC_LearnComposeAdditionalTheme
 
 class MainActivity : ComponentActivity() {
@@ -17,30 +21,38 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             JC_LearnComposeAdditionalTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
+                CreateHorizontalPager()
             }
         }
     }
 }
 
+@OptIn(ExperimentalFoundationApi::class)
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun CreateHorizontalPager() {
+
+    HorizontalPager(
+        pageCount = 10
+    ) { page ->
+        Card(
+            modifier = Modifier
+                .fillMaxWidth()
+                .size(140.dp)
+                .padding(20.dp)
+        ) {
+            Text(
+                text = "Page: $page",
+                modifier = Modifier.padding(12.dp)
+            )
+        }
+    }
 }
+
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     JC_LearnComposeAdditionalTheme {
-        Greeting("Android")
+        CreateHorizontalPager()
     }
 }
